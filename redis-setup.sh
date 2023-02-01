67ebc41027a8d382b6e381046d5a91bf4fab3ecf
foogaro <l.fugaro@gmail.com>
2023-02-01 14:48:33 +0100

    Renamed the file register-triggers.sh in redis-setup.sh just for naming convention.

--- redis-setup.sh
+++ redis-setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+set -e
+
+# redis-cli -x -h redis config set requirepass redis.2023
+
+# redis-server restart
+
+# redis-cli -x -h redis RG.JEXECUTE com.foogaro.data.cache.CatchAll < /tmp/redis-gears-for-caching-patterns-1.0.0.jar
 
 redis-cli -x -h redis RG.JEXECUTE com.foogaro.data.cache.PersonWriteBehind < /tmp/redis-gears-for-caching-patterns-1.0.0.jar
 redis-cli -x -h redis RG.JEXECUTE com.foogaro.data.cache.PersonWriteThrough < /tmp/redis-gears-for-caching-patterns-1.0.0.jar
 redis-cli -x -h redis RG.JEXECUTE com.foogaro.data.cache.PersonReadThrough < /tmp/redis-gears-for-caching-patterns-1.0.0.jar
 redis-cli -x -h redis RG.JEXECUTE com.foogaro.data.cache.PersonRefreshAhead < /tmp/redis-gears-for-caching-patterns-1.0.0.jar
+redis-cli -x -h redis RG.JEXECUTE com.foogaro.data.cache.PersonReadReplica < /tmp/redis-gears-for-caching-patterns-1.0.0.jar
+
