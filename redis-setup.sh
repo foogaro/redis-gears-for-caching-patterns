303f1ccfa69a2e42251fd3e8091ddde513713446
foogaro <l.fugaro@gmail.com>
2023-02-01 17:40:48 +0100

    Removed old things.

--- redis-setup.sh
+++ redis-setup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 
-# redis-cli -x -h redis config set requirepass redis.2023
-
-# redis-server restart
-
 # redis-cli -x -h redis RG.JEXECUTE com.foogaro.data.cache.CatchAll < /tmp/redis-gears-for-caching-patterns-1.0.0.jar
-
 redis-cli -x -h redis RG.JEXECUTE com.foogaro.data.cache.PersonWriteBehind < /tmp/redis-gears-for-caching-patterns-1.0.0.jar
 redis-cli -x -h redis RG.JEXECUTE com.foogaro.data.cache.PersonWriteThrough < /tmp/redis-gears-for-caching-patterns-1.0.0.jar
 redis-cli -x -h redis RG.JEXECUTE com.foogaro.data.cache.PersonReadThrough < /tmp/redis-gears-for-caching-patterns-1.0.0.jar
