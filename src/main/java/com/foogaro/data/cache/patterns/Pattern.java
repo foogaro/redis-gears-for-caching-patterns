ca870d46eef7996e344578916528637420a8bf5f
foogaro <l.fugaro@gmail.com>
2023-02-01 12:54:15 +0100

    Added the getReader() method to eventually override it in the concrete implementations.

--- src/main/java/com/foogaro/data/cache/patterns/Pattern.java
+++ src/main/java/com/foogaro/data/cache/patterns/Pattern.java
@@ -4,9 +4,20 @@ import gears.ExecutionMode;
 import gears.GearsBuilder;
 import gears.operations.OnRegisteredOperation;
 import gears.operations.OnUnregisteredOperation;
+import gears.readers.BaseReader;
+import gears.readers.KeysReader;
 
 public abstract class Pattern implements OnProcessEvent, OnRegisteredOperation, OnUnregisteredOperation {
 
+    public BaseReader<?> getReader() {
+        KeysReader reader = new KeysReader()
+                .setPattern(getKeyPattern())
+                .setNoScan(true)
+                .setReadValues(true);
+        if (getEventsType() != null) reader.setEventTypes(getEventsType());
+        if (getCommands() != null) reader.setCommands(getCommands());
+        return reader;
+    }
 
     @Override
     public void onRegistered(String registrationId) throws Exception {
@@ -22,5 +33,23 @@ public abstract class Pattern implements OnProcessEvent, OnRegisteredOperation, 
     public abstract String[] getEventsType();
     public abstract String getKeyPattern();
     public String[] getCommands() { return null; };
+
+    public String[] generateHSET(Object obj, String id) throws IllegalAccessException {
+        String hashName = obj.getClass().getSimpleName().toLowerCase();
+        var fields = obj.getClass().getDeclaredFields();
+        int redisCommandIndex = 0;
+        String[] redisCommand = new String[(fields.length*2) + 2];
+        redisCommand[redisCommandIndex++] = "HSET";
+        redisCommand[redisCommandIndex++] = hashName+":"+id;
+
+        for (int i = 0; i < fields.length; i++) {
+            var field = fields[i];
+            field.setAccessible(true);
+            redisCommand[redisCommandIndex++] = field.getName();
+            redisCommand[redisCommandIndex++] = field.get(obj).toString();
+        }
+
+        return redisCommand;
+    }
 
 }
