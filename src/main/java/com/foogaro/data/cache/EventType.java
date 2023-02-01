ebf0eb199a030e868150205b86965ab3cd4bff4d
foogaro <l.fugaro@gmail.com>
2023-02-01 12:55:47 +0100

    Changed the CDC event type code to xadd, becasue the CDC component is implemented using the Redis Stream data structure.

--- src/main/java/com/foogaro/data/cache/EventType.java
+++ src/main/java/com/foogaro/data/cache/EventType.java
@@ -2,7 +2,7 @@ package com.foogaro.data.cache;
 
 public enum EventType {
 
-    HSET("hset"), HGET("hget"), HMGET("hmget"), KEY_MISS("keymiss"), EXPIRED("expired"), CDC("cdc");
+    HSET("hset"), HGET("hget"), HMGET("hmget"), KEY_MISS("keymiss"), EXPIRED("expired"), CDC("xadd");
     private String eventType;
 
     EventType(String eventType) {
